809375a62e0d378cb804f3cd3eb6fd4c03c60533
Frank <fboucher@outlook.com>
2021-05-15 08:59:48 -0400

    starting experiment with claim

--- api/SecuredTest.cs
+++ api/SecuredTest.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Security.Claims;
 
 namespace Company.Function
 {
@@ -15,9 +16,41 @@ namespace Company.Function
         [FunctionName("SecuredTest")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "secured/SecuredTest")] HttpRequest req,
-            ILogger log)
+            ILogger log,
+            ClaimsPrincipal principal)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            bool isClaimValid = true;
+
+            if (principal == null)
+            {
+                log.LogWarning("No principal.");
+                isClaimValid = false;
+            }
+
+            if (isClaimValid && principal.Identity == null)
+            {
+                log.LogWarning("No identity.");
+                isClaimValid = false;
+            }
+
+            if (isClaimValid && !principal.Identity.IsAuthenticated)
+            {
+                log.LogWarning("Request was not authenticated.");
+                isClaimValid = false;
+            }
+
+            if (isClaimValid && principal.FindFirst(ClaimTypes.GivenName) is null)
+            {
+                log.LogError("Claim not Found");
+                isClaimValid = false;
+            }
+
+            if(isClaimValid)
+            {
+                var userId = principal.FindFirst(ClaimTypes.GivenName).Value;
+                log.LogInformation("Authenticated user {user}.", userId);
+            }
 
             string name = req.Query["name"];
 
